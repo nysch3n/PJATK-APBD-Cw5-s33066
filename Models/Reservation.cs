94b3d4b1283f8a41dffece9bdb620d4d2618a09a
Jakub Rębkowski <s33066@pjwstk.edu.pl>
2026-04-13 11:11:59 +0200

    dodanie modeli Room i Reservation z walidacja

---
+++ Models/Reservation.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Cw5.Models;
+
+public class Reservation : IValidatableObject
+{
+    public int Id { get; set; }
+
+    public int RoomId { get; set; }
+
+    [Required]
+    public string OrganizerName { get; set; } = string.Empty;
+
+    [Required]
+    public string Topic { get; set; } = string.Empty;
+
+    [Required]
+    public DateOnly Date { get; set; }
+
+    [Required]
+    public TimeOnly StartTime { get; set; }
+
+    [Required]
+    public TimeOnly EndTime { get; set; }
+
+    public string Status { get; set; } = string.Empty;
+
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+        if (EndTime <= StartTime)
+        {
+            yield return new ValidationResult("EndTime must be greater than StartTime.", [nameof(EndTime)]);
+        }
+    }
+}
